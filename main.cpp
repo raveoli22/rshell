0f6e4133d668c4af661cff3775693d59448ca808
Yuxuan Li <yli066@hammer.cs.ucr.edu>
2015-01-22 23:12:20 -0800

    changes made

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <cstring>
 #include <string>
+#include <vector>
 #include <unistd.h>
 #include <stdio.h>
 
 int main(){
     char hostname[200];
     char username[200];
+    std::vector <char*> commands;
     std::string userinput;
-    int result;
+
 
     bool end = false;
     gethostname(hostname, 200);
@@ -16,6 +19,15 @@ int main(){
     while (!end){
         std::cout<< username <<"@"<<hostname<<"$";
         getline(std::cin,userinput);
+        char *token = std::strtok(const_cast<char*>(userinput.c_str()), ";&|");
+        while (token != NULL){
+            commands.push_back(token);
+            token = std::strtok(NULL, ";&|");
+        }
+        for (int i = 0; i < commands.size(); i++){
+            std::cout<<commands.at(i) << std::endl;
+        }
+        commands.clear();
     }
 }
 
